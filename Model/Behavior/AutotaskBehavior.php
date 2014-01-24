58b636d66a5551fc8d534f4a53298f9a2bed1975
Coen Coppens <coen@coencoppens.nl>
2014-01-24 09:28:46 +0100

    Fixed a huge bug in the import script.
    Whenever you would query entities and get 500n+1 records, you would
    only get 1 record!

--- Model/Behavior/AutotaskBehavior.php
+++ Model/Behavior/AutotaskBehavior.php
@@ -38,6 +38,7 @@
 			 *	SubIssueType
 			 *	ServiceLevelAgreementID
 			 *	TicketType
+			 *	Stage
 			 *
 			 */
 			if (isset($this->_aPicklist[$sEntity])) {
@@ -96,24 +97,24 @@
 					<query>
 			';
 
-			if( !empty( $aQuery['conditions'] ) ) {
+			if (!empty($aQuery['conditions'])) {
 
 				// Every query consists of a set of conditions ($aDetails) grouped by the equality ($sEquality),
 				// i.e. 'Equals' or 'NotEqual'.
-				foreach ( $aQuery['conditions'] as $sEquality => $aDetails ) {
+				foreach ($aQuery['conditions'] as $sEquality => $aDetails) {
 
 					// Every field gets transformed to a proper XML string, i.e.
 					// QueueID ($sField) equals ($sEquality) 2233932 ($sExpression).
-					foreach ( $aDetails as $sField => $mExpressions ) {
+					foreach ($aDetails as $sField => $mExpressions) {
 
 						// Multiple values to match.
-						if( is_array( $mExpressions ) ) {
+						if (is_array($mExpressions)) {
 
 							$sXML .= '<condition>';
 
-								foreach ( $mExpressions as $iKey => $sExpression ) {
+								foreach ($mExpressions as $iKey => $sExpression) {
 
-									if( 0 == $iKey ) {
+									if (0 == $iKey) {
 										$sXML .= '<condition>';
 									} else {
 										$sXML .= '<condition operator="OR">';
@@ -146,28 +147,30 @@
 			';
 
 			try {
-				$oResponse = $this->oAutotask->query( array( 'sXML' => $sXML ) );
-			} catch ( SoapFault $fault ) {
-				$this->log( ' - Error occured while performing query: "' . $fault->faultcode .' - ' . $fault->faultstring . '"', 'cronjob' );
+				$oResponse = $this->oAutotask->query(array('sXML' => $sXML));
+			} catch (SoapFault $fault) {
+				$this->log(' - Error occured while performing query: "' . $fault->faultcode .' - ' . $fault->faultstring . '"', 'cronjob');
 				return false;
 			}
 
-			if( !empty( $oResponse->queryResult->EntityResults->Entity ) ) {
+			if (!empty($oResponse->queryResult->EntityResults->Entity)) {
 
 				// Only 1 result returned.
-				if( 1 == count( $oResponse->queryResult->EntityResults->Entity ) ) {
+				if (1 == count($oResponse->queryResult->EntityResults->Entity)) {
 
-					if( !empty( $oResponse->queryResult->EntityResults->Entity->id ) ) {
-						$this->_aResults = $oResponse->queryResult->EntityResults->Entity;
+					if (!empty($oResponse->queryResult->EntityResults->Entity->id)) {
+
+						$this->_aResults[] = $oResponse->queryResult->EntityResults->Entity;
 						$this->_iLastId = $oResponse->queryResult->EntityResults->Entity->id;
+
 					}
 
 				// Multiple results returned.
 				} else {
 
-					foreach ( $oResponse->queryResult->EntityResults->Entity as $oEntry ) {
+					foreach ($oResponse->queryResult->EntityResults->Entity as $oEntry) {
 
-						if( !empty( $oEntry->id ) ) {
+						if (!empty($oEntry->id)) {
 
 							$this->_aResults[] = $oEntry;
 							$this->_iLastId = $oEntry->id;
@@ -180,11 +183,11 @@
 
 				// The API has a limit of 500 results per request. If you get 500 there's probably some more
 				// to fetch :-)
-				if( 500 == count( $oResponse->queryResult->EntityResults->Entity ) ) {
+				if (500 == count($oResponse->queryResult->EntityResults->Entity)) {
 
-					$this->log( ' - Whoa, that\'s quite the amount of entries! Going for another 500, hang on..', 'cronjob' );
+					$this->log(' - Whoa, that\'s quite the amount of entries! Going for another 500, hang on..', 'cronjob');
 					$aQuery['conditions']['GreaterThan']['id'] = $this->_iLastId;
-					return $this->queryAutotask( $oModel, $sEntity, $aQuery );
+					return $this->queryAutotask($oModel, $sEntity, $aQuery);
 
 				} else {
 
@@ -204,65 +207,78 @@
 		}
 
 		private function getAutotaskLogin() {
-			
+
 			$aLogin = array(
-					'login' => Configure::read( 'Autotask.username' )
-				,	'password' => Configure::read( 'Autotask.password' )
-				,	'location' => Configure::read( 'Autotask.asmx' )
+					'login' => Configure::read('Autotask.username')
+				,	'password' => Configure::read('Autotask.password')
+				,	'location' => Configure::read('Autotask.asmx')
 			);
 
 			if(
-				empty( $aLogin['login'] )
+				empty($aLogin['login'])
 				||
-				empty( $aLogin['password'] )
+				empty($aLogin['password'])
 				||
-				empty( $aLogin['location'] )
+				empty($aLogin['location'])
 			) {
 
-				$this->log( 'I\'m not able to use the Autotask API if you don\'t provide your credentials (/var/www/app/Plugin/Autotask/Config/bootstrap.php).', 'cronjob' );
-				$this->log( 'I\'m not able to use the Autotask API if you don\'t provide your credentials (/var/www/app/Plugin/Autotask/Config/bootstrap.php).', 'error' );
+				$this->log('I\'m not able to use the Autotask API if you don\'t provide your credentials (/var/www/app/Plugin/Autotask/Config/bootstrap.php).', 'cronjob');
+				$this->log('I\'m not able to use the Autotask API if you don\'t provide your credentials (/var/www/app/Plugin/Autotask/Config/bootstrap.php).', 'error');
 				return false;
 
 			}
 			return $aLogin;
-			
+
 		}
+
 		public function checkConnectAutotask() {
-			
+
 			$oResponse = $this->oAutotask->getThresholdAndUsageInfo();
-			if(empty($oResponse->getThresholdAndUsageInfoResult->EntityReturnInfoResults->EntityReturnInfo->Message)) {
+			if (empty($oResponse->getThresholdAndUsageInfoResult->EntityReturnInfoResults->EntityReturnInfo->Message)) {
 				return false;
 			}
+
 			if(strpos($oResponse->getThresholdAndUsageInfoResult->EntityReturnInfoResults->EntityReturnInfo->Message, 'TimeframeOfLimitation')) {
 				return true;
 			}
+
 			return false;
+
 		}
+
 		public function connectAutotask() {
-			if(isset($this->oAutotask)) {
-				if(is_object($this->oAutotask)) {
+
+			if (isset($this->oAutotask)) {
+
+				if (is_object($this->oAutotask)) {
 					return true;
 				}
+
 			}
-			
+
 			$aLogin = $this->getAutotaskLogin();
+
 			if ($aLogin == false) { 
 				return false;
 			}
-			if ( !extension_loaded( 'soap' ) ) {
-				$this->log( 'SOAP is not available, unable to perform requests to the Autotask API.', 'cronjob' );
-				$this->log( 'SOAP is not available, unable to perform requests to the Autotask API.', 'error' );
+
+			if (!extension_loaded('soap')) {
+				$this->log('SOAP is not available, unable to perform requests to the Autotask API.', 'cronjob');
+				$this->log('SOAP is not available, unable to perform requests to the Autotask API.', 'error');
 				exit();
 			}
 
-			$this->oAutotask = new SoapClient( Configure::read( 'Autotask.wsdl' ), $aLogin );
+			$this->oAutotask = new SoapClient(Configure::read('Autotask.wsdl'), $aLogin);
+
 			if ($this->checkConnectAutotask() === true) {
 				return true;
 			}
+
 			else {
 				unset($this->oAutotask);
 				return false;
 			}
+
 		}
-		
+
 	}
